0e2a1f9514501e0ce563ff773e720c172b92a119
ViktorUserName <vityachishevich@gmail.com>
2022-09-08 21:04:57 +0300

    second complite

---
+++ src/components/services/index.js
@@ -0,0 +1,5 @@
+import Services from './services.js';
+
+export {
+  Services
+};
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {Head} from './components/header';
+import {Services} from './components/services';
+
 
 // const App = () => {
 //   return (
@@ -15,6 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Head />
+    <Services />
   </React.StrictMode>
 );
 
